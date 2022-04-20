9646e96ad079e53a3b33db6836bc8cc5d04b3ae2
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-20 02:08:36 -0700

    Time: 8 ms (90.70%), Space: 6 MB (33.96%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-        //change int to string
+        // change int to string
         string s = to_string(x);
         
-        //scan all characters
+        // scan all characters
         for(int i = 0, j = s.length() - 1; i <= j; i++, j--)
         {
-           if(s[i] != s[j]) //if each elements is different, return false
+           if(s[i] != s[j]) // if each elements is different, return false
                return false;
         }
         
-        return true; //else, return true
+        return true; // else, return true
     }
 };
