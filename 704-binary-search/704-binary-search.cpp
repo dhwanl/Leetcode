d8e83393827816f0bc93a8630ed894be9d1e1e78
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-20 13:33:18 -0700

    Time: 47 ms (58.95%), Space: 27.5 MB (92.51%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -2,29 +2,15 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        // call a helper function
-        return binarySearch(nums, target, 0, nums.size() - 1);
-    }
-
-// use the helper function to use recursion
-private:
-    int binarySearch(vector<int>& nums, int target, int low, int high)
-    {
-        // if low > high, then it fail to find the target in the vector
-        if(low <= high)
-        {
-            // find the midpoint to compare to target
-            int mid = (low + high) / 2;
-            
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] < target)
-                return binarySearch(nums, target, mid + 1, high);
-            else if(nums[mid] > target)
-                return binarySearch(nums, target, low, mid - 1);
-        }
+       // read all elements in vector and compare it to target
+       for(int i = 0; i < nums.size(); i++)
+       {
+           // if nums[i] == target, then return the index number
+           if(nums[i] == target)
+             return i;
+       }
         
-          return -1;
-        
+        // if fail, then return -1
+        return -1;
     }
 };
