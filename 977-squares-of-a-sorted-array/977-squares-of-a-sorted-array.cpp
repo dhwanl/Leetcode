94cfa91f4ef7509af7ac2f983d2d5e528b97d2be
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-21 13:44:16 -0700

    Time: 28 ms (93.34%), Space: 25.9 MB (81.99%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -5,21 +5,28 @@ public:
         int size, head, tail, tempIndex;
         
         size = nums.size();
+        
+        // make two pointers for the given vector
         head = 0;
         tail = size - 1;
+        
+        // a pointer for new vector;
         tempIndex = tail;
         
+        // declare a new vecotr
         vector<int> temp(size);
         
         for(; tempIndex >= 0;)
         {
             if(abs(nums[head]) > abs(nums[tail]))
             {
+                // square nums[head] if nums[head] > nums[tail]
                 nums[head] = nums[head] * nums[head];
                 temp[tempIndex--] = nums[head++];
                 
             }else{
                 
+                // square nums[tail] if nums[tail] > nums[head]
                 nums[tail] = nums[tail] * nums[tail];
                 temp[tempIndex--] = nums[tail--];
             }
