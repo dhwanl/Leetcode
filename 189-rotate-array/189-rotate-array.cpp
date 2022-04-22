c07fcf5b7b202936d8e4a2f274d67a73334ad3a9
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-22 00:42:27 -0700

    Time: 43 ms (41.57%), Space: 25.7 MB (11.31%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -6,7 +6,6 @@ public:
         int size = nums.size();
         vector<int> v(size);
         
-        // (i + k) % size
         // i.e 3 % 7 = 3 4 % 7 = 4 etc.
         for(int i = 0; i < size; i++)
             v[(i + k) % size] = nums[i];
