3c0f5ead586b4a12e7d620cbbd5b376fb068f657
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-22 00:34:39 -0700

    Time: 47 ms (30.11%), Space: 25.7 MB (24.25%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -5,9 +5,12 @@ public:
         int size = nums.size();
         vector<int> v(size);
         
+        // i.e 3 % 7 = 3 4 % 7 = 4 etc.
         for(int i = 0; i < size; i++)
             v[(i + k) % size] = nums[i];
         
+        
+        // copy the ordered array to the original vector
         for(int i = 0; i < size; i++)
             nums[i] = v[i];
     }   
