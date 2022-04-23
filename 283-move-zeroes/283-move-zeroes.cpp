81d55173ddca2ff22d222109fd6a4671ccc2982c
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-23 12:54:55 -0700

    Time: 35 ms (41.78%), Space: 19.3 MB (14.14%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
       
+        // declare two points and temp to change elements
         int ptr1, ptr2, temp, size;
         
+        // starts from 0 and 1 to compare
         ptr1 = 0;
         ptr2 = 1;
         
         size = nums.size();
         
+        // stop the function when there is no comparison
         if(size == 1) return;
         
         for(; ptr2 < size;)
         {
             
-            if(nums[ptr1] != 0) ptr1++;
-            if(nums[ptr2] == 0) ptr2++;
+            // find zero and non-zero numbers and exchange it
+            if(nums[ptr1] != 0) ptr1++; // increase ptr1 when it is not 0
+            if(nums[ptr2] == 0) ptr2++; // increase ptr2 when it is 0
             else{
                 
+                // change elements
                 temp = nums[ptr1];
                 nums[ptr1] = nums[ptr2];
                 nums[ptr2] = temp;
                 
-                ptr2++;
+                ptr2++; // increase ptr2 to move on
             }
                 
         }
