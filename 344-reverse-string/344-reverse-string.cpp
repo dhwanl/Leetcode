7a5fc7bbfe844aee6dffa0b2e48ae4b36447773c
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-23 14:52:53 -0700

    Time: 35 ms (31.57%), Space: 23.2 MB (42.16%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         
+        /*
+            1. make two pointers to reverse elements in the vector
+            2. using recursion to repeat the work
+        */
+        
+        // make two pointers
         int size, low, high;
         
         size = s.size();
         low = 0;
         high = size - 1;
         
+        // call recursion function
         recursion(s, low, high);
     
     }
@@ -15,14 +22,16 @@ public:
 private:
     void recursion(vector<char>& s, int low, int high)
     {
-        if(low >= high)
-            return;
+        if(low >= high) // set base case
+            return; // end recursion when it is done
         else
         {
+            // exchange each element
             char temp = s[low];
             s[low] = s[high];
             s[high] = temp;
             
+            // call the function itself again
             recursion(s, low + 1, high - 1);
         }
             
