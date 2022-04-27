51aff7c86c66fffc05d8f4dfb61df3af6938dee4
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-27 12:30:55 -0700

    Time: 2 ms (57.37%), Space: 7.2 MB (23.13%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -12,25 +12,32 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         
+        // use two pointers
         ListNode *ptr1, *ptr2;
         
+        // find the length of the node
         int size = 1;
         
+        // default
         ptr1 = ptr2 = head;
         
+        // find out the end of the node
         while(ptr2->next != nullptr)
         {
             ptr2 = ptr2 -> next;
             size++;
         }
      
+        // move one of the pointers into the middle
         for(int i = 0; i < (size / 2); i++)
         {
             ptr1 = ptr1 -> next;
         }
         
+        // put head into the middle
         head = ptr1;
         
+        // return head
         return head;
     }
 };
