1eb3f735e45a661b1f5b8fb83ad4125e6a30cf19
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-27 14:18:42 -0700

    Time: 8 ms (36.47%), Space: 10.7 MB (76.12%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -12,23 +12,33 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         
+        /*
+            1. find out the size of the node to calculate where the nth element is
+            2. use two pointers to read all nodes through
+                and connect nodes except for a nth node
+            3. use if else statement
+        */
+        
         int size = 0;
         
         ListNode *ptr1, *ptr2;
         
+        // default two pointers
         ptr1 = ptr2 = head;
         
+        // find the end of nodes and its size
         while(ptr2 != nullptr)
         {
             ptr2 = ptr2 -> next;
             size++;
         }
         
+        // return nothing
         if(size == 1 && n == 1) return head = nullptr;
-        else if(size <= n) return head->next;
+        else if(size <= n) return head->next; // first element must be taken out from the node
         else if(size > n){
             
-            ptr2 = head -> next;
+            ptr2 = head -> next; // reset ptr2
             
             for(int i = 1; i < size - n; i++)
             {
@@ -36,14 +46,12 @@ public:
                 ptr2 = ptr2->next;
             }
             
-            ptr2 = ptr2->next;
+            ptr2 = ptr2->next; // skip n-th element
             
-            ptr1->next = ptr2;
+            ptr1->next = ptr2; // connect node to the next element after n-th element
             
         }
         
-        
-        
-        return head;
+        return head; // return head
     }
 };
