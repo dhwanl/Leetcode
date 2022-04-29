35b1a401279ea27f3129052b312cfcce4406b6db
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-04-29 15:23:33 -0700

    Time: 3 ms (98.00%), Space: 8.3 MB (80.52%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         
-        
+        //Recursion
         /*        
             using recursion to reverse a linked list
                 set base case if head is empty or head next is empty
@@ -21,7 +21,7 @@ public:
                 to first print out two elements reversed at the end of linked list
             
             return a new linked list 
-        */
+        
         
         // base case of recursion
         if(head == NULL || head->next == nullptr) return head;
@@ -35,5 +35,39 @@ public:
         
         return small; // return small
         
+        */
+        
+        //loops
+        // remember a pointer refer to node itself, not a value
+        // and pointer can connect to each list
+        
+        /*
+            use loop with two pointers
+            each sequence loop scan only two elements and put them into one of pointers
+        */
+        
+        // in case when head or head->next is empty
+        if(head == nullptr || head->next == nullptr) return head;
+        
+        // two pointers
+        ListNode* ptr1 = head;
+        ListNode* ptr2 = head->next;
+        
+        // to return a pointer, make only first elements that will be printed at the end
+        // and connect ptr1 to ptr2 in each sequence i.e 1 / 2 1 / 3 2 1
+        ptr1->next = nullptr;
+        
+        // loop
+        while(ptr2 != nullptr)
+        {
+            // move the ptr2 into next at the end
+            ListNode* temp = ptr2->next;
+            ptr2->next = ptr1; // connect ptr1 to ptr2
+            ptr1 = ptr2; // give new head to ptr1
+            ptr2 = temp; // ptr2 move to next
+        }
+        
+        
+        return ptr1; // return ptr1
     }
 };
