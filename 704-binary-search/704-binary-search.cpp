2acc1716b9f24dabae6e74154ca63cf3794f3925
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-05-04 12:27:01 -0700

    Time: 48 ms (55.78%), Space: 27.4 MB (92.55%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-       /* 
+      
+        /*
+            1.use a loop
+            2.use a recursion
+        */
+        
+        
+        /*
        // read all elements in vector and compare it to target
        for(int i = 0; i < nums.size(); i++)
        {
@@ -13,6 +20,8 @@ public:
         // if fail, then return -1
         return -1;
         */
+        
+        // call a function for binary search
         return binarySearch(nums, target, 0, nums.size() - 1);
     
     }
@@ -20,9 +29,10 @@ public:
 private:
     int binarySearch(vector<int>& nums, int target, int low, int high)
     {
-        
+        // to prevent overflow
         if(low <= high)
         {
+            // find mid point in the vector
             int mid = low + (high - low) / 2;
             
             if(nums[mid] == target)
@@ -33,6 +43,7 @@ private:
                 return binarySearch(nums, target, low, mid - 1);
         }
         
+        // return -1 when a number does not exist in nums
         return -1;
     }
     
