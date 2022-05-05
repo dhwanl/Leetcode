582dcc1bcd0d2ee114b8b706b583d6c193054d7d
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-05-05 16:10:14 -0700

    Time: 0 ms (100.00%), Space: 6.9 MB (19.06%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -6,14 +6,18 @@ public:
     
     void push(int x) {
         
+        // put x into q2
         q2.push(x);
         
+        // if q1 is not empty, then add them up to q2
+        // to avoid all elements from q2 to q1
         while(!empty())
         {
             q2.push(q1.front());
             q1.pop();
         }
         
+        // swap q1 and q2
         queue<int> tmp = q1;
         q1 = q2;
         q2 = tmp;
@@ -21,6 +25,7 @@ public:
     
     int pop() {
         
+        // first value
         int result = top();
         q1.pop();
         
@@ -28,10 +33,12 @@ public:
         
     }
     
+    // return the first value in the queue because it is FIFO
     int top() {
         return q1.front();
     }
     
+    // check if q1 is empty
     bool empty() {
         return q1.empty();
     }
