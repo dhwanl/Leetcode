77e8d1587376c5f38a3ef6cdd6a8f95df78d8198
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-05-06 14:06:42 -0700

    Time: 42 ms (36.07%), Space: 13.9 MB (13.13%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -4,22 +4,27 @@ public:
 
         /*
             1. use a stack to store char, when there are k same characters
+                to make it more efficient, use a pair to store the value and the count
+                of each character
             2. delete them.
         */
-        string res = "";
-        stack<pair<char, int>> sta1;
         
-        for(char c : s)
+        string res = ""; // declare string to return
+        stack<pair<char, int>> sta1; // use a stack with pair
+        
+        for(char c : s) // iterate until string is empty
         {
+            // add new value to stack
+            // when either sta1 is empty or there is not the same value 
             if(!sta1.size() || sta1.top().first != c)
                 sta1.push({c, 1});
             else{
                 
-                sta1.push({c, sta1.top().second + 1});
+                sta1.push({c, sta1.top().second + 1}); // increase count
                 
                 if(sta1.top().second >= k)
                 {
-                    for(int i = 0; i < k; i++)
+                    for(int i = 0; i < k; i++) // remove the value that is repeted kth times
                         sta1.pop();
                 }
             }
@@ -27,14 +32,16 @@ public:
             
         }
         
+        // move stack into string
+        // remember stack is LILO
         while(sta1.size())
         {
             res += sta1.top().first;
             sta1.pop();
         }
         
-        reverse(res.begin(), res.end());
+        reverse(res.begin(), res.end()); // reverse string
         
-        return res;
+        return res; // return strinng
     }
 };
