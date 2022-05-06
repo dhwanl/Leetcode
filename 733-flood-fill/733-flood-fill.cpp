1fb9d63a2c786fed8c35136c75cdc7087f02792f
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-05-06 14:34:24 -0700

    Time: 19 ms (20.74%), Space: 14 MB (81.41%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    // recursive function
     void paintPixel(vector<vector<int>>& image, int sr, int sc, int center, int newColor)
     {
+        // base case
         if(sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() 
            || image[sr][sc] != center || image[sr][sc] == newColor) return;
         
-        image[sr][sc] = newColor;
+        image[sr][sc] = newColor; // change to new color
         paintPixel(image, sr - 1, sc, center, newColor);
         paintPixel(image, sr + 1, sc, center, newColor);
         paintPixel(image, sr, sc - 1, center, newColor);
@@ -21,9 +23,9 @@ public:
             then, recurses on neighboring pixels.
         */
         
-        int center = image[sr][sc];
+        int center = image[sr][sc]; // find the middle as a starting point
         
-        paintPixel(image, sr, sc, center, newColor);
+        paintPixel(image, sr, sc, center, newColor); // call the function for recursion
         
         return image;
         
