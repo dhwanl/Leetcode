266e351f65403be0e9ad0967dc2533df4f1258b3
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-05-07 15:53:03 -0700

    Time: 99 ms (48.19%), Space: 38 MB (97.40%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -5,6 +5,8 @@ public:
         int size = nums.size();
         int min = nums[0];
         
+        // find minimum 
+        // i.e. find i in i < j < k
         for(int i = 1; i < size; i++)
         {
            if(min > nums[i])
@@ -13,17 +15,21 @@ public:
         
         stack<int> sta;
         
+        // find the second largest one, j
         for(int i = size - 1; i >= 0; i--)
         {
+            // find k and compare j
             if(nums[i] < min)
                 return true;
             
+            // change min to j
             while(!sta.empty() && nums[i] > sta.top())
             {
                 min = sta.top();
                 sta.pop();
             }
             
+            // if stack is empty, then put a temporary element into stack
             sta.push(nums[i]);
         }
         
