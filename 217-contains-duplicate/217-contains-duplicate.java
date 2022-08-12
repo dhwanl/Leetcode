ead632b106363c838def5a40aa81884d07738bf6
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-08-11 20:16:46 -0700

    Time: 8 ms (88.07%), Space: 54.9 MB (91.37%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,6 +1,23 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
        
+        /*
+        For the final keyword, it won't necessarily improve the performance. 
+        Guys from Sun declare that the JVM can detect and indicate final 
+        even when developer does not specify it. 
+        But for some cases, especially for some JVMs, final does improve performance, 
+        for example, string concatenation will be done during compile time 
+        thus improve performance.
+        
+        For short, you should use final for clearer design/code purpose,
+        not for performance purpose.
+        */
+        
+        /*
+        Set: interface
+        HashSet: implementation
+        TreeSet: implementation [sorted]
+        */
         final Set<Integer> hashSet = new HashSet<Integer>();
         
         for(int num : nums){
