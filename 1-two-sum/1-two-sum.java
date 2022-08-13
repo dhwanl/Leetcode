33da14a3bd7345aede733c0ab8045eaeaf68b66c
dhwanl <100495958+dhwanl@users.noreply.github.com>
2022-08-12 19:59:51 -0700

    Time: 4 ms (77.28%), Space: 45.4 MB (48.69%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,6 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-               
+        
+        /*
+Solution using HashMap
+
+    We need to find 2 numbers a, b so that a + b = target.
+    We need a HashMap datastructure to store elements in the past, let name it map.
+    The idea is that we iterate b as each element in nums, 
+    we check if we found a (where a =    target - b) in the past.
+    If a exists in map then we already found 2 numbers a and b, so that a + b = target, 
+    just output their indices.
+    Else add b to the map.
+
+Complexity
+
+  Time: O(N), where N <= 10^4 is number of elements in the array nums.
+  Space: O(N)
+        */
         // Stores the data in (Key, Value) pairs
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         
